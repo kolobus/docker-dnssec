d26ca571587d1ab366d7f3ccc4b64bf3a7a220ea
Claudio Walser <claudio.walser@srf.ch>
2019-02-19 16:34:24 +0100

    dns works so far, need to implement dnssec now

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,18 @@ for var in $(compgen -A variable | grep -E 'FORWARDER_[0-9]{1,3}'); do
 done
 sed -i -E "s|<FORWARDER>|${FORWARDER}|g" /etc/bind/named.conf
 
-
 mkdir -p /var/bind/zones
 if [ -d "/domains" ]
 then
 	for zonefile in $(ls /domains/*.yaml); do
 		domain=$(echo $zonefile | sed -E "s|/domains/||g" | sed -E "s|.yaml||g")
-		/mkzone $zonefile >> /var/bind/zones/$domain
+		/mkzone $zonefile >> /var/bind/zones/${domain}.zone
+
+		echo "zone \"${domain}\" {" >> /etc/bind/named.conf.zones
+		echo "  type master;" >> /etc/bind/named.conf.zones
+		echo "  file \"zones/${domain}.zone\";" >> /etc/bind/named.conf.zones
+		echo "};" >> /etc/bind/named.conf.zones
+		echo "" >> /etc/bind/named.conf.zones
 		echo $zonefile
 		echo $domain
 	done
