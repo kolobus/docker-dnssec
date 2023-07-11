4be01ce072eb676156fbec0342d2fcdd4d0e6a7f
Claudio <info@gitcd.io>
2023-07-11 11:05:56 +0200

    bit better example

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,9 +2,13 @@
 
 docker build --rm -t net-sec/dnssec ./
 docker rm -f dnssec
+
+# a real salt can be derrived from
+# head -c 1000 /dev/random | sha1sum | cut -b 1-16
 docker run -itd \
-	-v $(pwd)/exampleDomains:/domains \
-	-v $(pwd)/domainKeys:/var/bind/keys \
-	-p 53:53/tcp \
-	-p 53:53/udp \
+	-v $(pwd)/exampleDomains:/domains:z \
+	-v $(pwd)/domainKeys:/var/bind/keys:z \
+    -e SALT=very-secret-salty-string \
+	-p 5353:53/tcp \
+	-p 5354:53/udp \
 	--name dnssec net-sec/dnssec
