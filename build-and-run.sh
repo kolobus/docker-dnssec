8ab2c2e8e4cfebafc0252a2913426b82fa316e9c
Claudio <info@gitcd.io>
2023-07-12 21:46:50 +0200

    Map on 53 locally, therefore run as sudo

--- build-and-run.sh
+++ build-and-run.sh
@@ -8,10 +8,10 @@ sudo chown -R 100099 domainKeys
 
 # a real salt can be derived from
 # head -c 1000 /dev/random | sha1sum | cut -b 1-16
-podman run -itd \
+sudo podman run -itd \
 	-v $(pwd)/exampleDomains:/domains:ro \
 	-v $(pwd)/domainKeys:/var/bind/keys:z \
     -e SALT=4aa69cc7a5d54175 \
     -e DNSSEC=true \
-	-p 5353:53 \
+	-p 53:5353 \
 	--name dnssec net-sec/dnssec
